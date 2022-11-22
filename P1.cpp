a38bbbb47d4bdbd284510ce2ccb7cfa03304351e
Sajjad Bukhari <90232785+Sajjad-Bukhari@users.noreply.github.com>
2022-11-22 22:15:33 +0500

    Add files via upload

---
+++ P1.cpp
@@ -0,0 +1,20 @@
+// Problem # 01
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num1, num2, sum;
+    
+    cout << "Input # 01: ";
+    cin >> num1;
+    
+    cout << "Input # 02: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+    cout << "Sum: " << sum << endl; 
+
+    return 0;
+}
